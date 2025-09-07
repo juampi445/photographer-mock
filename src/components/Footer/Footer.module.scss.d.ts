d4de2b974095458281f9686c51633ef91243b9bb
unknown <jpcuadrelli@gmail.com>
2025-09-06 23:48:16 -0300

    fix

---
+++ src/components/Footer/Footer.module.scss.d.ts
@@ -0,0 +1,12 @@
+declare const styles: {
+  readonly footer: string;
+  readonly container: string;
+  readonly footerContent: string;
+  readonly footerSection: string;
+  readonly socialLinks: string;
+  readonly contactInfo: string;
+  readonly footerBottom: string;
+  readonly footerLinks: string;
+};
+
+export default styles;
